0820cdef96dad9fd4e8d1da4eb7c7f4e305c9ba5
Nathan2801 <john.2801.oliver@gmail.com>
2025-04-20 23:38:24 -0300

    Don't allow moving when not colliding

--- common/movable.gd
+++ common/movable.gd
@@ -1,9 +1,10 @@
-extends Node
+extends Node2D
 class_name Movable
 
 var _active := false
 var _being_hovered := false
 var _impulse_origin := Vector2.ZERO
+var _impulse := Vector2.ZERO
 
 @export var _range := 400:
 	set(value):
@@ -13,19 +14,12 @@ var _impulse_origin := Vector2.ZERO
 var _min_range := Vector2(-_range, -_range)
 var _max_range := Vector2(+_range, +_range)
 
-var _delay := Timer.new()
-@export var _delay_time := 0.8
-
 @onready var _target: RigidBody2D = get_parent()
 
 func _ready():
 	_target.input_pickable = true
 	_target.mouse_entered.connect(_hover)
 	_target.mouse_exited .connect(_unhover)
-	
-	_delay.one_shot = true
-	_delay.wait_time = _delay_time
-	add_child(_delay)
 	
 func _hover() -> void:
 	_being_hovered = true
@@ -34,17 +28,19 @@ func _unhover() -> void:
 	_being_hovered = false
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
+	if event is InputEventMouseMotion:
+		_impulse = event.position - _impulse_origin
+		_impulse = _impulse.clamp(_min_range, _max_range)
+	elif event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				_impulse_origin = event.position
-			elif _active and _delay.is_stopped():
-				var mouse := Vector2(event.position)
-				var impulse := mouse - _impulse_origin
-				impulse = impulse.clamp(_min_range, _max_range)
-				_target.apply_impulse(impulse * -1 * _target.mass)
-				_impulse_origin = Vector2.ZERO
-				_delay.start()
+			elif _target.get_contact_count() > 0:
+				_apply_impulse()
+
+func _apply_impulse():
+	_target.apply_impulse(_impulse * -1 * _target.mass)
+	_impulse_origin = Vector2.ZERO
 
 func active() -> void:
 	_active = true
